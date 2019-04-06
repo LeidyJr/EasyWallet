a9a93e55985a00f3812f51ee8010cdfe57b4e384
Julian <julian.anacona@correounivalle.edu.co>
2019-04-06 15:53:01 -0500

    Registrar transacciones via ingreso o egreso

--- apps/transacciones/urls.py
+++ apps/transacciones/urls.py
@@ -9,6 +9,8 @@ app_name='transacciones'
 urlpatterns = [
 
     url(regex=r"^registrar$", view=crear_transaccion, name="registrar_transaccion"),
+    url(regex=r"^registrar_ingreso$", view=crear_ingreso, name="registrar_ingreso"),
+    url(regex=r"^registrar_egreso$", view=crear_egreso, name="registrar_egreso"),
     url(regex=r"^listado_ingresos$", view=mis_ingresos, name="listado_de_ingresos"),
     url(regex=r"^listado_egresos$", view=mis_egresos, name="listado_de_egresos"),
  

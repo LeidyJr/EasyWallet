f95b5f59af2657eb5c26eeada4f223a277fa7670
Juan David Tello <tello.juan@correounivalle.edu.co>
2019-04-06 01:41:53 -0500

    settings.py

--- ds2/settings.py
+++ ds2/settings.py
@@ -165,14 +165,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
 
 
 DATE_INPUT_FORMATS = ['%d-%m-%Y']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static'),
 )
